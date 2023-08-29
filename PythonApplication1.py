b054f73440ad2a55d94ff1dd0998c517750bd007
torre <torre@MSI>
2023-08-29 13:15:44 -0500

    Add project files.

---
+++ PythonApplication1.py
@@ -0,0 +1,17 @@
+#First time using python
+print("1.Add");
+print("2. Subtract");
+print("3. Multiply");
+print("4. Divide");
+print("5. Exit Program");
+choice=int(input("enter your choice:"));
+if (choice>=1 and choice<=4):
+        print("Enter two numbers: ");
+num1=int(input());
+num2=int(input());
+if choice==1:
+    res=num1+num2;
+print("Result = ",res);
+if choice==2:
+    res=num1+num2;
+print("result = ",res);
